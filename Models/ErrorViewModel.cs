4726bb83645ba3eaae630643baf088d4e006ebb4
AmanWho101 <Amleaz2005@gmail.com>
2022-08-09 16:08:04 +0300

    crud

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Crud_mvcdotnet.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
